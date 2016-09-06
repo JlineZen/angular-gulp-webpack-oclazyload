97e7ccb6a1a96b79d12383109b7d3595b13cfbb9
jianglu <244981288@qq.com>
2016-09-06 23:25:03 +0800

    update index route module and update gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,50 @@
 var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var cleanCss = require('gulp-clean-css');
+var del = require('del');
+var cache = require('gulp-cache');
+var swallowError = function(error) {
+    console.log(error.toString());
+    this.emit('end');
+};
+
+/**
+ * @desc use gulp to build webpack task
+ */
+
+gulp.task('webapck', function() {
+    return gulp.src('./src/app.js')  // webpack entry file
+        .pipe(webpack(require('./webpack.config.js'))) // webpack config
+        .on('error', swallowError) // handle error when gulp watch
+        .pipe(gulp.dest('./dist/js'));
+});
+
+/**
+ * @desc minify css file
+ */
+
+gulp.task('minify-css', function() {
+    gulp.src('./src/style/*.css')
+        .pipe(cleanCss())
+        .on('error', swallowError)
+        .pipe(gulp.dest('./dist/style'));
+});
+
+
+/**
+ * @desc build watch task
+ */
+gulp.task('watch', ['webpack', 'minify-css'], function() {
+    gulp.watch('src/**/*.js', ['webapck']);
+    gulp.watch('src/**/*.css', ['minify-css']);
+});
+
+/**
+ * clean dist
+ */
+
+gulp.task('clean', function() {
+    return del.sync('./dist');
+});
+
+gulp.task('default', ['clean', 'watch']);
